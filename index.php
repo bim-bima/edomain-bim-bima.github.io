8093bc3d9e8f978b768be8d4d577f9403601c789
bim-bima <pahlawanmalas@gmail.com>
2022-08-31 08:56:08 +0700

    commit awal

---
+++ index.php
@@ -0,0 +1,388 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="assets/style/style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Source+Sans+Pro:wght@300;400;600;700&display=swap" rel="stylesheet">
+	<title>edomain</title>
+</head>
+<body>
+	<div class="container">
+	<div class="header cf">
+		<div class="konten-atas">
+			<div class="ka">
+				<div class="judul-ma">Grow Your Businnes Online</div>
+				<div class="menu-atas">
+		  			<a href="#">Malaysia</a>
+					<a href="#">Announcements</a>
+					<a href="#">Contact Us</a>
+					<a href="#">WhatsApp</a>
+			  		<a href="#">Uptime</a>
+			  		<a href="#">Client Login</a>
+				</div>
+			</div>
+		</div>
+		<div class="konten-tengah">
+			<div class="ka">
+				<a href="#"><img class="logo-1" src="assets/img/logo.png"></a>
+				<div class="menu-tengah">
+		  			<a class="clr-blue" href="#">Domains</a>
+					<a href="#">Hosting</a> 
+					<a href="#">Design</a>
+					<a href="#">Servers</a>
+					<a href="#">Clouds</a>
+					<a href="#">Solutions</a>
+					<a href="#">Marketing Tools</a>
+					<a href="#">Digital Service</a> 
+					<a href="#"><img class="search" src="assets/img/search.png"></a>		
+				</div>
+			</div>
+		</div>
+		<div class="konten-bawah">
+  			<a class="judul-mb" href="#">Domains</a>
+  			<div class="mb">
+  				<a href="#">Domain Name Search</a>
+  				<a href="#">MY Domain Names</a>
+  				<a href="#">Premium Domain Names</a>
+  				<a href="#">International Domain Extansion</a>
+  				<a href="#">Domain Transfer</a>
+  				<a href="#">More</a>	
+  			</div>
+  		</div>
+	</div>
+		<div class="hero-img cf">
+			<div></div>
+			<div class="content-right">
+				<h4>Domain Registration Malaysia</h4>
+				<h1>It All Start With A <br>Domain Name</h1><br>
+				<p>Choose from more than 400 different domain extansions.WHOIS Domain<br>Protection Include</p>
+				<input type="text" name="" class="input-domain" placeholder="Your domain name"><button class="button" role="button">Check Domain</button>
+			</div>
+		</div>
+		<div class="section">
+			<div class="">
+				<h1>Dream</h1>
+				<p class="title">I dreamt.I saw it coming and I made it happen by talking a tiny step to decide on my very own domain.</p>
+			</div>
+			<div class="card">		
+				<div class="card-left color-cl">
+					<div class="txt">
+						<img class="logo" src ="assets/img/globe.png">
+						<p>Domains</p><br>
+						<h2>Top Level Domains</h2><br>
+						<p>Choose from more than 400 different extensions. WHOIS<br> domain protection is included to ensure that your privacy is not invaded <br>by the public.</p>
+						<button class="button-2" >Find Out More</button>
+						<p class="link"><a href="#">Start Domain Search</a></p>
+					</div>
+				</div>
+				<div class="card-right clr">
+					<div class="txt">
+						<img class="logo" src ="assets/img/globe.png">
+						<img class="logo" src ="assets/img/discount.png">
+						<p>Domains</p><br>
+						<h2>.MY Domains</h2><br>
+						<p>Localise your business online with .MY Domains. Rank higher on Google<br>when customers search your business in Malaysia. Get your perfect<br>domain now.</p>
+						<button class="button-2" >Find Out More</button>
+						<p class="link"><a href="#">Start Domain Search</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="card cf">
+				<div class="card-left clr">
+					<div class="txt">
+						<img class="logo" src ="assets/img/globe.png">
+						<p>Domains</p><br>
+						<h2>Domains Transfer</h2><br>
+						<p>Transfer your domain to Edomain and enjoy a lower renewal rate. We<br>
+						offer the lowest renewal rate you can find in the market. Tranfer to us<br>today.</p>
+						<button class="button-2" >Find Out More</button>
+						<p class="link"><a href="#">Transfer Now</a></p>
+					</div>
+				</div>
+				<div class="card-right clr-cr">
+					<div class="txt">
+						<img class="logo" src ="assets/img/globe.png">
+						<p>Domains</p><br>
+						<h2>Domains Proxy Service</h2><br>
+						<p>Planning to buy foreign country's domain name without NRIC? with Edomain.<br>It's made possible by registering for our domain proxy service.</p>
+						<button class="button-2" >Find Out More</button>
+						<p class="link"><a href="#">Start Domain Search</a></p>
+					</div>
+				</div>
+			</div>	
+		</div>
+		<div class="content-bttm">
+			<div class="benner">
+				<!-- <div class="cf"></div> -->
+				<div class="space">
+					<div class="title-mt">
+						<h1 class="judul-bnnr">More Than A Domain Name</h1>
+						<p>Having a domain name with edomain you priceless features and benefits.</p>
+					</div>
+					<div class="my-brand"></div>
+					<div class="bttm-bnnr">
+						<div class="bttm-bnnr-up">
+							<h4>Domain Lock Included</h4>
+							<p>Stop unauthorised parties drom stealling away your<br>domain with our domain lock.</p>
+						</div>
+						<div class="bttm-bnnr-up">
+							<h4>Domain Privacy Protection</h4>
+							<p>Free domain privacy protection is include without<br>additional charges.</p>
+						</div>
+						<div class="bttm-bnnr-up">
+							<h4>FREE DNS Management</h4>
+							<p>You have  the freedom to edit your DNS records with<br>our beginner friendly dashboard.</p>
+						</div>
+						<div class="bttm-bnnr-up">
+							<h4>Protect Your Brand</h4>
+							<p>Prevent competitors from usein your brand name<br>online for their own benefits.</p>
+						</div>
+						<div class="bttm-bnnr-up">
+							<h4>Boost Customer Trust</h4>
+							<p>Let your potential customers feel safe when making<br>trancations on your website</p>
+						</div>
+						<div class="bttm-bnnr-up">
+							<h4>Manage On The Go</h4>
+							<p>Manage domain names,renewls and more through<br>your mobile devices.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="qust">
+			<h1 class="faqs">FAQs</h1>
+			<div class="box-faqs">
+				<div class="content-faqs">
+					<h4>What is domain?</h4>
+					<p>Domain definition: Domain name is the name of your website or in common we call it website URL/ web address.Every website must have a domain name to<br>be searchable on the web.</p>
+				</div>
+			</div>
+			<div class="box-faqs">
+				<div class="content-faqs">
+					<h4>What is the proces of domain registration?</h4>
+				</div>
+			</div>
+			<div class="box-faqs">
+				<div class="content-faqs">
+					<h4>Can I deregister my domain after domain registration?</h4>
+				</div>
+			</div>
+			<div class="box-faqs">
+				<div class="content-faqs">
+					<h4>How much is a domain name worth?</h4>
+				</div>
+			</div>
+			<div class="box-faqs">
+				<div class="content-faqs">
+					<h4>Why is domain impotant?</h4>
+				</div>
+			</div>
+			<div class="box-faqs btm-50">
+				<div class="content-faqs buttm-50">
+					<h4>Is domain name case sensitive?</h4>
+				</div>
+			</div>
+		</div>
+		<div class="review">
+			<div class="jarak">
+				<div class="judul-rvw">
+					<h1>Why Choose<br>Edomain</h1><br>
+					<p>Your complete Cloud,Digital and eCommers solution provider</p>
+				</div>
+				<div class="br">
+					<div class="box-rvw">
+						<div class="content-review">
+							<h4>Sea's Trusted and<br>Leading Brand</h4><br>
+							<p>Known and recognised as Southeast Asia's leading cloud,eCommerce and digital solutions provider.trust by over 160,000 customers globally.</p>
+						</div>
+					</div>
+				<div class="box-rvw">
+					<div class="content-review">
+						<h4>More Than A Web<br>Hosting Provider</h4><br>
+						<p>Known and recognised as Southeast Asia's leading cloud,eCommerce and digital solutions provider.trust by over 160,000 customers globally.</p>
+					</div>
+				</div>
+				<div class="box-rvw">
+					<div class="content-review">
+						<h4>The Latest is Here<br>at Exabyts</h4><br>
+						<p>Known and recognised as Southeast Asia's leading cloud,eCommerce and digital solutions provider.trust by over 160,000 customers globally.</p>
+					</div>
+				</div>
+				<div class="box-rvw">
+					<div class="content-review">
+						<h4>Proven by 100%<br>Client Satisfaction</h4><br>
+						<p>Known and recognised as Southeast Asia's leading cloud,eCommerce and digital solutions provider.trust by over 160,000 customers globally.</p>
+					</div>
+				</div>
+				<div class="cf"></div>
+				<div class="rating">
+					<div class="box-rate">
+						<div class="content-rating">
+							<p>4.7</p>
+						</div>
+						<img src="assets/img/star.PNG">
+						<p class="opacity">1,560 reviews</p>
+						<a class="buttom-3" href="#">Write a Review</a>
+					</div>
+					<div class="cf"></div>
+					<div class="coment">
+						<img class="cmnt-1" src="assets/img/coment-1.PNG">
+						<img class="cmnt-2" src="assets/img/coment.PNG">
+						<img class="cmnt-1" src="assets/img/coment-1.PNG">
+					</div>
+					<div class="cf"></div>
+				</div>
+				<div class="img-right">
+					<img src="assets/img/ads-1.PNG">
+				</div>
+				</div>
+			</div>
+		</div>
+		<div class="contact">
+			<div class="box-contact">
+				<div class="judul-contact">
+					<h5>Reachable 24/7</h5>
+					<h1>Always here<br>for you</h1>
+					<div class="contact-left">
+						<li><a href="#">WhatsApp</a></li>
+						<li><a href="#">Live Chat</a></li>
+						<li><a href="#">KnowledgeBase</a></li>
+					</div>
+					<div class="contact-right">
+						<li><a href="#">Phone Support</a></li>
+						<li><a href="#">Email</a></li>
+						<li><a href="#">Blog</a></li>
+					</div>
+				</div>
+				<div class="payment">
+					<div class="our-pay">
+						<h5>Our Payment Method</h5>
+					</div>
+					<div class="logo-pay">
+						<a href="#"><img src="assets/img/visa.png"></a>
+						<a href="#"><img src="assets/img/paypal.png"></a>
+						<a href="#"><img src="assets/img/visa.png"></a>
+						<a href="#"><img src="assets/img/paypal.png"></a>
+						<a href="#"><img src="assets/img/visa.png"></a>
+						<a href="#"><img src="assets/img/paypal.png"></a>
+					</div>
+					<div class="logo-pay">
+						<a href="#"><img src="assets/img/paypal.png"></a>
+						<a href="#"><img src="assets/img/visa.png"></a>
+						<a href="#"><img src="assets/img/paypal.png"></a>
+						<a href="#"><img src="assets/img/visa.png"></a>
+						<a href="#"><img src="assets/img/paypal.png"></a>
+						<a href="#"><img src="assets/img/visa.png"></a>
+					</div>
+					<div class="logo-pay">
+						<a href="#"><img src="assets/img/visa.png"></a>
+						<a href="#"><img src="assets/img/paypal.png"></a>
+						<a href="#"><img src="assets/img/visa.png"></a>
+						<a href="#"><img src="assets/img/paypal.png"></a>
+						<a href="#"><img src="assets/img/visa.png"></a>
+						<a href="#"><img src="assets/img/more.png"></a>
+					</div>
+					<div class="logo-lingkaran">
+						<a href="#"><img src="assets/img/logo-l1.PNG"></a>
+						<a href="#"><img src="assets/img/logo-l2.PNG"></a>
+						<a href="#"><img src="assets/img/logo-l3.PNG"></a>
+						<a href="#"><img class="logo-l4" src="assets/img/logo-l4.PNG"></a>
+					</div>
+				</div>
+				<div class="cf"></div>
+			</div>
+		</div>
+		<div class="footer">
+			<div class="footer-1">
+				<div class="footer-left">
+					<a href="#"><img class="get" src="assets/img/logo.png"></a>
+					<div class="cf"></div>
+					<a href="#"><img class="get" src="assets/img/appstore-1.png"></a>
+					<div class="cf"></div>
+					<a href="#"><img class="get" src="assets/img/google-play.png"></a>
+					<!-- <div class="cf"></div> -->
+				</div>
+				<div class="footer-right">
+					<h4>Resources</h4>
+					<a href="#"><p>Promo</p></a>
+					<a href="#"><p>Reviews</p></a>
+					<a href="#"><p>Announcement</p></a>
+					<a href="#"><p>Knowledge Base</p></a>
+					<a href="#"><p>Control Panel</p></a>
+					<a href="#"><p>Money-Back Guarantee</p></a>
+					<a href="#"><p>Rewards</p></a>
+					<a href="#"><p>Billing Information</p></a>
+					<a href="#"><p>View all Products</p></a>
+				</div>
+				<div class="footer-right">
+					<h4>Company</h4>
+					<a href="#"><p>About Edomain</p></a>
+					<a href="#"><p>Our Awards&Achievments</p></a>
+					<a href="#"><p>Talents Wanted</p></a>
+					<a href="#"><p>Edomain Logo Download</p></a>
+					<a href="#"><p>Edomain App Download</p></a>
+					<a href="#"><p>Edomain Data Center</p></a>
+					<a href="#"><p>Edomain Blog</p></a>
+					<a href="#"><p>Edomain Book</p></a>
+					<a href="#"><p>Edomain Events</p></a>
+					<a href="#"><p>Customer Testimonials</p></a>
+					<a href="#"><p>Legal Information</p></a>
+					<a href="#"><p>Corporate Governance</p></a>
+					<a href="#"><p>Contact Use</p></a>
+				</div>
+				<div class="footer-right">
+					<h4>Partner Us</h4>
+					<a href="#"><p>Edomain Cockroach Startup Program</p></a>
+					<a href="#"><p>Edomain Sponsorship</p></a>
+					<a href="#"><p>Edomain Affiliate Program</p></a>
+					<a href="#"><p>Edomain Reseller Partner Programme</p></a>
+					<a href="#"><p>Edomain Reseller Listing</p></a>
+					<a href="#"><p>Cloud Backup Partner Programme</p></a>
+					<a href="#"><p>Edomain Designer (EDC)</p></a>
+					<a href="#"><p>Edomain SME Digitalsation Program</p></a>
+					<a href="#"><p>EasyStore</p></a>
+					<a href="#"><p>EasyParcel</p></a>
+					<a href="#"><p>Write For Us</p></a>
+				</div>
+				<div class="cf"></div>
+				<div class="footer-bttm">
+					<div class="footer-bttm-l">
+						<h4>Subscribe Newsletter</h4>
+						<p>Subscribe to Exabytes Newsletter for online tips,<br>events and latest promotion!</p>
+					</div>
+					<input type="text" name="" class="full-name cf" placeholder="Full Name"><input type="email" name="" class="email" placeholder="Email">
+					<button class="bttn-subs" role="button">Subscribe</button>
+					<div class="sosial-media ">
+						<div class="cf"></div>
+						<a href="#"><img class="" src="assets/img/facebook.png"></a>
+						<a href="#"><img class="" src="assets/img/instagram.png"></a>
+						<a href="#"><img class="" src="assets/img/twitter.png"></a>
+						<a href="#"><img class="" src="assets/img/youtube.png"></a>
+						<a href="#"><img class="" src="assets/img/linkedin.png"></a>
+					</div>
+				</div>
+				<div class="cf"></div>
+				<div class="footer-bttm-2">
+					<div class="footer-partner">
+						<a href="#"><img class="logo-partner" src="assets/img/sitelock01.png"></a>
+						<a href="#"><img class="logo-partner" src="assets/img/google-partner01.png"></a>
+						<a href="#"><img class="logo-partner" src="assets/img/meta01.png"></a>
+						<a href="#"><img class="logo-partner" src="assets/img/sitelock01.png"></a>
+						<a href="#"><img class="logo-partner" src="assets/img/google-partner01.png"></a>
+						<a href="#"><img class="logo-partner" src="assets/img/meta01.png"></a>
+						<a href="#"><img class="logo-partner" src="assets/img/sitelock01.png"></a>
+						<a href="#"><img class="logo-partner" src="assets/img/google-partner01.png"></a>
+						<a href="#"><img class="logo-partner" src="assets/img/meta01.png"></a>
+					</div>
+					<div class="copy">
+						<p>Copyright©2022 Edomain Network Sdn.Bhd.(576092-T)|All Trademarks Are The Property of Their Respoctive Owner.|Service Tax No.P11-1809-32000073.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
